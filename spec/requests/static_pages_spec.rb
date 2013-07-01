2111ac0492ef6feb53eaebfd704a7b4bb580a4f3
diguage <leejun119@gmail.com>
2013-07-01 17:02:58 +0800

    增加测试用例

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,4 +16,11 @@ describe "StaticPages" do
     end
   end
 
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+
 end
