ad556f9859a408a02100c5d14b9bd6137256255d
diguage <leejun119@gmail.com>
2013-07-01 20:08:24 +0800

    测试标题，为下面从重构做准备

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,12 @@ describe "StaticPages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the right title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', 
+      	                        :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
@@ -14,12 +20,24 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', 
+      	                        :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', 
+      	                        :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 
