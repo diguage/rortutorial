0577e904920694f9f73e67269e8cc159f85a1df1
diguage <leejun119@gmail.com>
2013-07-01 21:05:04 +0800

    使用插值将测试代码中的重复代码提取出来

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
   	it "should have the content 'Sample App'" do
@@ -10,8 +12,7 @@ describe "StaticPages" do
 
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', 
-      	                        :text => "Ruby on Rails Tutorial Sample App | Home")
+      page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -23,8 +24,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', 
-      	                        :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -36,8 +36,7 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', 
-      	                        :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
 
@@ -49,8 +48,7 @@ describe "StaticPages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', 
-      	                        :text => "Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 
