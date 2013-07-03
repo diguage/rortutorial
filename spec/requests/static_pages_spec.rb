8998fa12554c512630ab42f9800bd7d9bfb3cc70
diguage <leejun119@gmail.com>
2013-07-03 17:59:25 +0800

    测试网站链接，同时修正发现的错误

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,25 @@ describe "StaticPages" do
     it_should_behave_like "all static pages"
   end
 
+  it "should have the right links on the layout" do
+    visit root_path
+    
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('')
+
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+
+  end
+
 end
