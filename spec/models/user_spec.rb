beedfc355cdf7f9c427fa9fc5ab8fabee037b9d5
diguage <leejun119@gmail.com>
2013-07-03 21:59:45 +0800

    对User建立测试用例

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before { @user = User.new(name: "dgg", email: "dgg@dgg.com") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) } 
 end
