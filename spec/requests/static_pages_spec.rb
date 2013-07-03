9fd340bf64e5c9df77273278d9897e9b7d7364d0
diguage <leejun119@gmail.com>
2013-07-03 16:13:03 +0800

    使用spec/support/utilities.rb来测试标题

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,11 +8,10 @@ describe "StaticPages" do
   
   describe "Home page" do
     before { visit root_path }
-  	it { should have_selector('h1', :text => 'Sample App') }
-
-    it { should have_selector('title', :text => base_title) }
-
-    it { should_not have_selector('title', :text => '| Home') }
+    
+  	it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
