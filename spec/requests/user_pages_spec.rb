509a767784cd31d1e09e230c785e3c6f01c47350
diguage <leejun119@gmail.com>
2013-07-03 16:36:05 +0800

    添加测试代码

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get user_pages_index_path
-      response.status.should be(200)
-    end
+
+  subject { page }
+
+  describe "Signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
   end
 end
