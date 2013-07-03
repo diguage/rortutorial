d975fc31b7e9ed1b22ff10b600ed13732ee8804b
diguage <leejun119@gmail.com>
2013-07-04 00:02:17 +0800

    完成第六章，验证email保存为小写

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -110,4 +110,15 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+
+  end
 end
