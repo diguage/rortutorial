42f09b131639bdd93054106b8d6bdb07ef232d0f
diguage <leejun119@gmail.com>
2013-07-03 23:13:37 +0800

    对Users表增加密码列，并增加测试用例

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130703145747) do
+ActiveRecord::Schema.define(:version => 20130703151204) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
