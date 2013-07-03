f2987c99b8b4673816e9023c65eb43dcf39a8447
diguage <leejun119@gmail.com>
2013-07-03 16:24:40 +0800

    重构测试代码，修正错误

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,22 +17,22 @@ describe "StaticPages" do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_selector('h1', :text => 'Help') }
-    it { should have_selector('title', :text => "#{base_title} | Help") }
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_selector('h1', :text => 'About Us') }
-    it { should have_selector('title', :text => "#{base_title} | About Us")}
+    it { should have_selector('h1', text: 'About Us') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact" do
-    before { contact_path }
+    before { visit contact_path }
 
-    it { should have_selector('h1', :text => 'Contact') }
-    it { should have_selector('title', :text => "#{base_title} | Contact") }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 
 end
