4a669b6912328707c28469ae6178cf7833087258
diguage <leejun119@gmail.com>
2013-07-03 22:07:07 +0800

    添加对User的name属性的验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  validates :name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,10 +13,19 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "dgg", email: "dgg@dgg.com") }
+  before do
+    @user = User.new(name: "dgg", email: "dgg@dgg.com") 
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) } 
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
 end
