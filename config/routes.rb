26522c04bdde4d8340d2e2b7c9aaa3d4ada78655
diguage <leejun119@gmail.com>
2013-07-03 16:36:18 +0800

    修改路由规则

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rtutorial::Application.routes.draw do
 
   root to: 'static_pages#home'
 
+  match '/signup', to: 'users#new'
+
   match '/help', to: 'static_pages#help'
-
   match '/about', to: 'static_pages#about'
-
   match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
