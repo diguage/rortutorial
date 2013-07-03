176ca413fcb77e4eff68f35da89832a17f4f55c1
diguage <leejun119@gmail.com>
2013-07-03 22:53:20 +0800

    增加email唯一性验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name
 
   validates :name, presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,4 +58,13 @@ describe User do
       end
     end
   end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
 end
