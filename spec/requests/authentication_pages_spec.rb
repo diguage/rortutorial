7b38a9dc0bc3b4048ff2c64a7852ee7ecaeb4719
diguage <leejun119@gmail.com>
2013-07-05 00:38:27 +0800

    添加登陆页面的测试·

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe "AuthenticationPages" do
-  describe "GET /authentication_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get authentication_pages_index_path
-      response.status.should be(200)
-    end
+  
+  subject { page }
+
+  describe "Signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
   end
 end
