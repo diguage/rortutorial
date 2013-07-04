226a9e0756514161d6f68543a28c88aa47354175
diguage <leejun119@gmail.com>
2013-07-04 11:25:30 +0800

    添加用户展示页面，并测试

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Dgg"
+    email    "leejun119@gmail.com"
+    password "adai"
+    password_confirmation "adai"
+  end	
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,14 @@ describe "UserPages" do
     before { visit signup_path }
 
     it { should have_selector('h1', text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('title', text: 'Sign up') }
+  end
+
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
   end
 end
