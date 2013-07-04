c354f0161a82a18a63bf40a4fef64bcccc4021ed
diguage <leejun119@gmail.com>
2013-07-04 12:38:37 +0800

    完成注册页面展示

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,4 +18,28 @@ describe "UserPages" do
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+         expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Dgg"
+        fill_in "Email",        with: "leejun119@gmail.com"
+        fill_in "Password",     with: "adai123"
+        fill_in "Confirmation", with: "adai123"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
