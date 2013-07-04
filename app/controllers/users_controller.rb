72c05d49baa9cb1e11c3798d8b8190df96ef098c
diguage <leejun119@gmail.com>
2013-07-04 12:59:34 +0800

    设定注册失败时的错误提示同时定义样式

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])  	
   end
+
+  def create
+    @user = User.new(params[:user])  	
+
+    if @user.save
+    else
+      render 'new'
+    end
+  end
 end
