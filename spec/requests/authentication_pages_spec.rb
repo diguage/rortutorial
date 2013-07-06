af8354228fcaf657c2798f153ee1ab1ce43db70e
diguage <leejun119@gmail.com>
2013-07-06 17:58:14 +0800

    修改发现的错误

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,7 +30,7 @@ describe "AuthenticationPages" do
       let(:user) { FactoryGirl.create(:user) }
       before do
         fill_in "Email", with: user.email 
-        fill_in "Password", with: user.Password
+        fill_in "Password", with: user.password
         click_button "Sign in"
       end
 
