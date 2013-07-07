5c6fc193e9b2ffa3940b615785a2f05b8dfc8bab
diguage <leejun119@gmail.com>
2013-07-07 16:58:38 +0800

    增加修改的Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,4 +20,22 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id]) 
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a success update
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def signed_in_user
+      redirect_to signin_path, notice: "Please sign in" unless signed_in?
+    end
 end
