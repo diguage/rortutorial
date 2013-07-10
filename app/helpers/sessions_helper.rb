c812ebf71556b70f5a6b1fc6b7875603a915485d
diguage <leejun119@gmail.com>
2013-07-10 08:51:55 +0800

    添加存储和跳转原来URL的方法

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -24,4 +24,13 @@ module SessionsHelper
     self.current_user = nil
     cookies.delete(:remember_token)  	
   end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)    
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
 end
