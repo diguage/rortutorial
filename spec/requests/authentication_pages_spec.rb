4702e9e483a7d279da02b99bbe794e70ff29e9a9
diguage <leejun119@gmail.com>
2013-07-10 08:51:26 +0800

    添加跳转还原来页面的测试方法

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -65,6 +65,21 @@ describe "AuthenticationPages" do
           specify { response.should redirect_to(signin_path) }
         end
       end
+
+      describe "when attempting to visit a protected page" do
+        before do 
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.password 
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Edit user')
+          end
+        end
+      end
     end
 
     describe "as wrong user" do 
