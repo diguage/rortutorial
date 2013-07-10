c24de5823c6aa5f5d127d4c1d5192fbd3f40ca60
diguage <leejun119@gmail.com>
2013-07-10 11:21:25 +0800

    添加用户列表的Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user, only:[:edit, :update]
 
   def new
@@ -35,6 +35,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.all  
   end
 
   private
