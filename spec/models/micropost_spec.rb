10f262ab6466704878417da1278fcad5f32d1ea7
diguage <leejun119@gmail.com>
2013-07-11 01:15:27 +0800

    增加测试用例

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Micropost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    # This code is wrong!
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
+
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
 end
