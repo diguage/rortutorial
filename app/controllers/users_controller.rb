576cb5fb48a76f85ec6285f4536e64fc630a1988
diguage <leejun119@gmail.com>
2013-07-10 17:16:56 +0800

    增加删除方法

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only:[:edit, :update]
+  before_filter :admin_user, only: :destroy
 
   def new
     @user = User.new
@@ -41,6 +42,12 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
   private
     def signed_in_user
       unless signed_in?
@@ -52,5 +59,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])      
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
