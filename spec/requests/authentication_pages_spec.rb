4ed093cc1c2c326b2da0f9808ec4127ab82fe1cf
diguage <leejun119@gmail.com>
2013-07-10 21:20:20 +0800

    增加跳转测试

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -84,6 +84,19 @@ describe "AuthenticationPages" do
           it "should render the desired protected page" do
             page.should have_selector('title', text: 'Edit user')
           end
+
+          describe "when signing in again" do
+            before do
+              visit signin_path
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password 
+              click_button "Sign in"
+            end
+
+            it "should render the default (profile) page" do 
+              page.should have_selector('title', text: user.name)
+            end
+          end
         end
       end
 
