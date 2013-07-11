9a614fa2ae52be128fc7479eb83bcb1ae6d221fe
diguage <leejun119@gmail.com>
2013-07-11 16:05:15 +0800

    显示微博内容

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])  	
+    @user = User.find(params[:id]) 
+    @microposts = @user.microposts.paginate(page: params[:page]) 	
   end
 
   def create
