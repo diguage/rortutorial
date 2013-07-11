eb07e909dcc823da168bf9dc4f1f534f03d8f74a
diguage <leejun119@gmail.com>
2013-07-11 19:10:07 +0800

    实现创建微博的Action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,7 +4,14 @@ class MicropostsController < ApplicationController
   def index  	
   end
 
-  def create  	
+  def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy  	
