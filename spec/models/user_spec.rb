d3ad2499c83193578a264e6401dd067a04a829f7
diguage <leejun119@gmail.com>
2013-07-11 11:47:55 +0800

    测试微博排序，以及关联删除

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -137,4 +137,27 @@ describe User do
 
     its(:remember_token) { should_not be_blank }
   end
+
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+  end
 end
