01a08db4164ebfe9702f2389de582dd0307dba13
diguage <leejun119@gmail.com>
2013-07-11 23:00:13 +0800

    增加feed方法，用于动态展示微博

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64      
