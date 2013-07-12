a0c72c57201e3a746f2a631ef809a64f8f5c9677
diguage <leejun119@gmail.com>
2013-07-12 10:31:34 +0800

    增加对Relationship关联的测试

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,6 +32,7 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
 
   it { should be_valid }
   it { should_not be_admin }
