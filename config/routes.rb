fbc0435d35a12974f07e4d2df94b8d31a0414d2a
diguage <leejun119@gmail.com>
2013-07-12 13:36:13 +0800

    增加关注和粉丝的路由规则，同时添加测试

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rtutorial::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions,   only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,6 +31,17 @@ describe "StaticPages" do
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "followers/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link('0 following', href: following_user_path(user)) }
+        it { should have_link('1 following', href: followers_user_path(user)) }
+      end
     end
   end
 
