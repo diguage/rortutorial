ade27aa8bedcc0ec88d7697f1cb92dab04563512
diguage <leejun119@gmail.com>
2013-07-12 10:40:44 +0800

    添加被关注用户测试以及建立与被关注用户之间的关联

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,6 +33,7 @@ describe User do
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
 
   it { should be_valid }
   it { should_not be_admin }
