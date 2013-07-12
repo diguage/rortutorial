4d36d07474da295b96a3b17dfc0b65363509bacf
diguage <leejun119@gmail.com>
2013-07-12 10:47:35 +0800

    测试关注用到的方法以及实现这些方法

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,14 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user)    
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64      
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,6 +34,8 @@ describe User do
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
   it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -172,5 +174,16 @@ describe User do
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
     end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
   end
 end
