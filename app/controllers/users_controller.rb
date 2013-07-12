8493387632ae92ae2d079bc52424f1a0cdfc1752
diguage <leejun119@gmail.com>
2013-07-12 18:09:58 +0800

    实现粉丝和关注展示所需的模板以及Action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,20 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
     def correct_user
       @user = User.find(params[:id])      
