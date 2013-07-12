f4138d89d09f0d04223ea6ab1e7778b2c44ea781
diguage <leejun119@gmail.com>
2013-07-12 18:09:28 +0800

    关注和粉丝展示页面的测试

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -70,6 +70,16 @@ describe "AuthenticationPages" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
       end
 
       describe "when attempting to visit a protected page" do
