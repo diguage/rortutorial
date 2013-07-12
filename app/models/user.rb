535a1a7cd2e1fb55199c9ab93b5e2d3ec373b663
diguage <leejun119@gmail.com>
2013-07-12 10:32:55 +0800

    增加对Relationship的关联

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
