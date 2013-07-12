4ce6179c26a595dc01ba213724aef5f71e1b5643
diguage <leejun119@gmail.com>
2013-07-12 22:46:40 +0800

    使用子查询优化后的列表

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,7 +9,9 @@ class Micropost < ActiveRecord::Base
   default_scope order: 'microposts.created_at DESC'
 
   def self.from_users_followed_by(user)
-    followed_user_ids = user.followed_user_ids
-    where("user_id IN (?) OR user_id = ?", followed_user_ids,user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+           followed_user_ids: followed_user_ids, user_id: user)
   end
 end
